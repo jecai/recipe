6c8668e67aa7024987456f15cbbaba83f4b11adf
Jenn Cai <jcai1@alumni.cmu.edu>
2017-10-04 21:55:13 -0700

    Comment out old recipe data classes

--- src/main/java/org/recipe/models/data/RecipeFieldData.java
+++ src/main/java/org/recipe/models/data/RecipeFieldData.java
@@ -1,37 +1,37 @@
-package org.recipe.models.data;
-
-import org.recipe.models.RecipeField;
-
-import java.util.ArrayList;
-
-public class RecipeFieldData<T extends RecipeField> {
-
-    private ArrayList<T> allFields = new ArrayList<>();
-
-    public ArrayList<T> findAll() {
-        return allFields;
-    }
-
-    public T findById(int id) {
-        for (T item : allFields) {
-            if (item.getId() == id)
-                return item;
-        }
-
-        return null;
-    }
-
-    public void add(T item) {
-        allFields.add(item);
-    }
-
-    T findByValue(String value) {
-        for (T item : allFields) {
-            if (item.contains(value))
-                return item;
-        }
-
-        return null;
-    }
-
-}
+//package org.recipe.models.data;
+//
+//import org.recipe.models.RecipeField;
+//
+//import java.util.ArrayList;
+//
+//public class RecipeFieldData<T extends RecipeField> {
+//
+//    private ArrayList<T> allFields = new ArrayList<>();
+//
+//    public ArrayList<T> findAll() {
+//        return allFields;
+//    }
+//
+//    public T findById(int id) {
+//        for (T item : allFields) {
+//            if (item.getId() == id)
+//                return item;
+//        }
+//
+//        return null;
+//    }
+//
+//    public void add(T item) {
+//        allFields.add(item);
+//    }
+//
+//    T findByValue(String value) {
+//        for (T item : allFields) {
+//            if (item.contains(value))
+//                return item;
+//        }
+//
+//        return null;
+//    }
+//
+//}
