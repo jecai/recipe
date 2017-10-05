08b35ec80abedf9a7230bbb7527de571b1385c6e
Jenn Cai <jcai1@alumni.cmu.edu>
2017-10-04 22:04:27 -0700

    Update Home/Recipe controllers to use DAO

--- src/main/java/org/recipe/controllers/RecipeController.java
+++ src/main/java/org/recipe/controllers/RecipeController.java
@@ -1,11 +1,12 @@
 package org.recipe.controllers;
 
 import org.recipe.models.Recipe;
-import org.recipe.models.data.RecipeData;
-import org.recipe.models.forms.RecipeForm;
+import org.recipe.models.data.RecipeDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -15,36 +16,39 @@ import javax.validation.Valid;
 @RequestMapping(value = "recipe")
 public class RecipeController {
 
-    private RecipeData recipeData = RecipeData.getInstance();
+    @Autowired
+    private RecipeDao recipeDao;
 
     // The detail display for a given Recipe at URLs like /job?id=17
     @RequestMapping(value = "", method = RequestMethod.GET)
     public String index(Model model, int id) {
 
         //get the Recipe with the given ID and pass it into the view
-        model.addAttribute("recipe", recipeData.findById(id));
+        model.addAttribute("recipe", recipeDao.findOne(id));
         return "recipe-detail";
     }
 
     @RequestMapping(value = "add", method = RequestMethod.GET)
     public String add(Model model) {
+        model.addAttribute("title", "Add Recipe");
         model.addAttribute(new Recipe());
         return "new-recipe";
     }
 
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String add(Model model, @Valid Recipe recipe, Errors errors) {
+    public String add(Model model, @ModelAttribute @Valid Recipe recipe, Errors errors) {
 
         // TODO #6 - Validate the RecipeForm model, and if valid, create a
         // new Recipe and add it to the recipeData data store. Then
         // redirect to the job detail view for the new Recipe.
 
         if (errors.hasErrors()) {
+            model.addAttribute("title", "Add Recipe");
             model.addAttribute(recipe);
             return "new-recipe";
         }
 
-        recipeData.add(recipe);
+        recipeDao.save(recipe);
         return "redirect:/recipe/?id=" + Integer.toString(recipe.getId());
 
     }
