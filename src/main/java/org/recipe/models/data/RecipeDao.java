08434b6f69ec73237802b277558d23d65b551c71
Jenn Cai <jcai1@alumni.cmu.edu>
2017-10-04 22:03:59 -0700

    Create persistent Recipe class

---
+++ src/main/java/org/recipe/models/data/RecipeDao.java
@@ -0,0 +1,17 @@
+package org.recipe.models.data;
+
+import org.recipe.models.Recipe;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+
+/**
+ * Created by Jenn Cai
+ * 10/4/17
+ */
+
+@Repository
+@Transactional
+public interface RecipeDao extends CrudRepository<Recipe,Integer> {
+}
