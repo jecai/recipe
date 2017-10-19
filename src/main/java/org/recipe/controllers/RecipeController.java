39b8fba2039359632a1c21529c426ec7402ddea8
Wilson Chan <Wilson_chan789@yahoo.com>
2017-10-19 02:45:07 -0700

    Session fixes

--- src/main/java/org/recipe/controllers/RecipeController.java
+++ src/main/java/org/recipe/controllers/RecipeController.java
@@ -55,19 +55,20 @@ public class RecipeController extends AbstractController {
     }
 
     @RequestMapping(value = "edit/{recipeId}", method = RequestMethod.GET)
-    public String displayEditForm(Model model, @PathVariable int recipeId) {
+    public String displayEditForm(Model model, @PathVariable int recipeId, HttpServletRequest request) {
         Recipe recipe = recipeDao.findOne(recipeId);
         model.addAttribute("recipe", recipe);
         model.addAttribute("title", "Edit Recipe: " +
                 recipe.getName());
         model.addAttribute("recipeId", recipeId);
+        model.addAttribute("sessionOn", isSessionActive(request.getSession()));
         return "edit";
     }
 
     @RequestMapping(value = "edit", method = RequestMethod.POST)
     public String processEditForm(Model model, int recipeId,
                                   @ModelAttribute @Valid Recipe newRecipe,
-                                  Errors errors) {
+                                  Errors errors, HttpServletRequest request) {
 
         Recipe recipe = recipeDao.findOne(recipeId);
 
@@ -85,6 +86,7 @@ public class RecipeController extends AbstractController {
         recipe.setCalorie(newRecipe.getCalorie());
         recipe.setImageUrl(newRecipe.getImageUrl());
         recipeDao.save(recipe);
+        model.addAttribute("sessionOn", isSessionActive(request.getSession()));
         return "redirect:/recipe/?id=" + Integer.toString(recipe.getId());
     }
 }
