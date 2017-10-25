7165563fcbd3f0ce5b22f8d89de615b8610c068d
Jenn Cai <jcai1@alumni.cmu.edu>
2017-10-25 00:54:24 -0700

    User pages with links to each

---
+++ src/main/java/org/recipe/controllers/UserController.java
@@ -0,0 +1,31 @@
+package org.recipe.controllers;
+
+import org.recipe.models.User;
+import org.recipe.models.data.RecipeDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping(value = "user")
+public class UserController extends AbstractController {
+
+    @Autowired
+    private RecipeDao recipeDao;
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public String userPage(Model model,  int id, HttpServletRequest request) {
+        User user = userDao.findOne(id);
+        model.addAttribute("title", user.getUsername() + "'s Recipes");
+        model.addAttribute("recipes", recipeDao.findByAuthor(user));
+        model.addAttribute("sessionOn", isSessionActive(request.getSession()));
+        model.addAttribute("isAuthor",
+                getUserFromSession(request.getSession()) == user);
+        return "list-recipes";
+    }
+
+}
